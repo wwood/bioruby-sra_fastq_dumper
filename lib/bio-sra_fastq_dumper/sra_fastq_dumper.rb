86c2cdd5ae2717fe1ddf491415cd61685eded69c
Ben J Woodcroft <donttrustben@gmail.com>
2012-06-14 22:40:46 +1000

    add untested code, but implementation is done I think

--- lib/bio-sra_fastq_dumper/sra_fastq_dumper.rb
+++ lib/bio-sra_fastq_dumper/sra_fastq_dumper.rb
@@ -1,3 +1,41 @@
-module BioSraFastqDumper
+require 'tmpdir'
 
+module Bio
+  class FastqDumper
+    # Given a path to an sra or sra-lite format file,
+    # go into a new temporary directory and convert that sra/sra-lite
+    # file into a fastq file in that temporary directory.
+    # Requires a block to be given - that block is passed
+    # the path to the fastq file after chdir'ing to the tmpdir
+    def self.dump_in_tmpdir(fastq_filename)
+      # Make sure that the fastq file exists, and collect its absolute path so soft-linking works later 
+      sra = File.absolute_path(fastq_filename)
+      raise unless File.exist?(sra)
+      
+      current_directory = Dir.getwd
+      Dir.mktmpdir do |dir|
+        Dir.chdir dir
+        
+        # Create a symlink to the fastq file passed as a parameter
+        local_path = File.pathname(sra)
+        File.symlink sra, local_path
+        
+        # Run fastq-dumper from the SRA toolkit
+        command = [
+          'fastq-dumper',
+          local_path
+        ]
+        Bio::Command.call_command_open3(command) do |stdin, stdout, stderr|
+          err = stderr.read
+          raise err if err != ''
+        end
+        
+        # Let the user use the dumped fastq file
+        yield local_path
+        
+        # Change back to the original directory
+        Dir.chdir current_directory
+      end
+    end
+  end
 end
